846b82e0ab9badb8eafec2d34f878e803c74dc40
hscokies <hello@hscokies.xyz>
2026-03-13 20:34:30 +0500

    feat: add task2

--- TaskTwo/Program.cs
+++ TaskTwo/Program.cs
@@ -1,11 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using TaskTwo;
+using TaskTwo.models;
 
 internal class Program
 {
     public static void Main(string[] args)
     {
-        
+        Pyramid[] pyramids =
+        [
+            new(3, 4, 60, 10),
+            new(5, 5, 45, 7),
+            new(6, 8, 30, 9),
+        ];
+
+        foreach (var pyramid in pyramids)
+        {
+            // Вызов метода Volume пропускаю т.к. он вызывается в ToString
+            Console.WriteLine(pyramid.ToString());
+        }
     }
 }
