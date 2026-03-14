75db987e42dd6ead5956ba125a827aec5dc572ae
hscokies <hello@hscokies.xyz>
2026-03-14 22:56:15 +0500

    chore: remove unused class

--- Shared/Geometry/Primitives/Point3D.cs
+++
@@ -1,9 +0,0 @@
-namespace Shared.Geometry.Primitives;
-
-public readonly record struct Point3D(double X, double Y, double Z)
-{
-    public override string ToString()
-    {
-        return $"({X}, {Y}, {Z})";
-    }
-}
